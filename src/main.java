29c8a7a5fb22d0464bb2c20e3eb9e4edb5c92be0
Ayush Kumar Rai <ayushkumarrai@shalinis-air.lan>
2022-07-01 12:30:32 -0500

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class main {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter first number- ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter mathematical operation (+,-,*,/,^) ");
+        char chr= sc.next().charAt(0);
+        int method = chr;
+
+        System.out.print("Enter second number- ");
+        int b = sc.nextInt();
+
+        if (method == 43) {
+
+        }
+    }
+}
